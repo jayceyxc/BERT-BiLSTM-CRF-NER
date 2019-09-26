a8d4f4c148d7721470225782942423860d9e570d
xinluomed_yuxuecheng <xinluomed_yuxuecheng@git.cloud.tencent.com>
2019-09-26 15:59:45 +0800

    1. 启动时设置端口号

--- start_ner.sh
+++ start_ner.sh
@@ -5,5 +5,5 @@ bert-base-serving-start \
     -bert_model_dir /workspace/yuxuecheng/tf_workspace/bert_base_models/chinese_L-12_H-768_A-12 \
     -model_pb_dir /workspace/yuxuecheng/tf_workspace/bert_out/BERT_NER \
     -port 8800 \
-    -port-out 8801 \
+    -port_out 8801 \
     -mode NER
